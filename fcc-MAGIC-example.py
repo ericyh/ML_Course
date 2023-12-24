4a6274ffac1136f98a197a5c1fff8601c302e89f
ericyh <ericyh53201595@gmail.com>
2023-12-24 23:15:11 +1100

    ML Course

---
+++ fcc-MAGIC-example.py
@@ -0,0 +1,7 @@
+#import pandas as pd
+#import matplotlib.pyplot as plt
+
+cols = ("fLength", "fWidth", "fSize", "fCone", "fCone1", "fAsym", "fM3Long", "M3Trans", "fAlpha")
+# df = pd.read_csv("magic04.data", name = cols)
+# df.head()
+print("Hey")
