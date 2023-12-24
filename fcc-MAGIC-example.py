ef14036b8fd7cb365e2c20e966f61eb5c67df192
ericyh <ericyh53201595@gmail.com>
2023-12-24 23:16:11 +1100

    Testing

--- fcc-MAGIC-example.py
+++ fcc-MAGIC-example.py
@@ -4,4 +4,4 @@
 cols = ("fLength", "fWidth", "fSize", "fCone", "fCone1", "fAsym", "fM3Long", "M3Trans", "fAlpha")
 # df = pd.read_csv("magic04.data", name = cols)
 # df.head()
-print("Hey")
+print("hI")
